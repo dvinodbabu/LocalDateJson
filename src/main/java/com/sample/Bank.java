6818d62b341ced6cee0232d96cad64de387e536c
dvinodbabu <devarajan.vinodbabu@gmail.com>
2020-12-22 20:18:59 +0530

    removed unused imports

--- src/main/java/com/sample/Bank.java
+++ src/main/java/com/sample/Bank.java
@@ -1,11 +1,11 @@
 package com.sample;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.google.gson.annotations.SerializedName;
 import org.threeten.bp.LocalDate;
-import org.threeten.bp.format.DateTimeFormatter;
+
 
 public class Bank {
     @JsonSerialize(using = ToStringSerializer.class)
